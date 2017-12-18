54c98be57e91f6cf09f3e259c13689220b0c519f
Will Clark <willdavidc@gmail.com>
2017-12-18 07:18:44 -0500

    Add promise rejection logging

--- utils/logger.js
+++ utils/logger.js
@@ -37,7 +37,7 @@ const fileExists = (fileName) => {
         fs.stat(fileName, (err, data) => {
             resolve(!err);
         });
-    }); 
+    });
 };
 
 const appendFile = (path, data) => {
