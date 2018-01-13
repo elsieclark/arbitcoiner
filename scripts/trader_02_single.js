2a444791d4fd5f44048129ab7eb0d8810e8d98b8
Will Clark <willdavidc@gmail.com>
2018-01-13 11:58:40 -0500

    Fix syntax error

--- scripts/trader_02_single.js
+++ scripts/trader_02_single.js
@@ -257,18 +257,18 @@ const makeTrade = (soldCoin, boughtCoin, frozenStatus) => {
         `\n        Projected final amount: ${frozenStatus[boughtCoin].balance + (0.9975 * frozenStatus[soldCoin].balance / rate)}`,
         `\n\n       `, frozenStatus, '\n');
 
-    Log.info('Gamma2', `${boughtCoin}_${soldCoin}`, 1/rate, frozenStatus.soldCoin.balance);
-    Log.info('Delta2', typeof `${boughtCoin}_${soldCoin}`, typeof 1/rate, typeof frozenStatus.soldCoin.balance);
+    Log.info('Gamma2', `${boughtCoin}_${soldCoin}`, 1/rate, frozenStatus[soldCoin].balance);
+    Log.info('Delta2', typeof `${boughtCoin}_${soldCoin}`, typeof 1/rate, typeof frozenStatus[soldCoin].balance);
 
     return queue.push({ flags: [`private_${soldCoin}`], priority: 11 }, () => {
         Log.info(`Actually executing ${soldCoin} -> ${boughtCoin} trade`);
         if (soldCoin === 'BTC' || (soldCoin === 'ETH' && boughtCoin !== 'BTC')) {
             console.log('Alpha');
-            return polo.buy(`${soldCoin}_${boughtCoin}`, rate, frozenStatus.soldCoin.balance/rate, false, true, false);
+            return polo.buy(`${soldCoin}_${boughtCoin}`, rate, frozenStatus[soldCoin].balance/rate, false, true, false);
         } else {
-            Log.info('Gamma', `${boughtCoin}_${soldCoin}`, 1/rate, frozenStatus.soldCoin.balance*0.0001);
-            Log.info('Delta', typeof `${boughtCoin}_${soldCoin}`, typeof 1/rate, typeof frozenStatus.soldCoin.balance);
-            return polo.sell(`${boughtCoin}_${soldCoin}`, 1/rate, frozenStatus.soldCoin.balance*0.0001, false, true, false);
+            Log.info('Gamma', `${boughtCoin}_${soldCoin}`, 1/rate, frozenStatus[soldCoin].balance*0.0001);
+            Log.info('Delta', typeof `${boughtCoin}_${soldCoin}`, typeof 1/rate, typeof frozenStatus[soldCoin].balance);
+            return polo.sell(`${boughtCoin}_${soldCoin}`, 1/rate, frozenStatus[soldCoin].balance*0.0001, false, true, false);
         }
     });
 };
