9da39b2909bbe1f32300128d9fbba885833d4d30
Will Clark <willdavidc@gmail.com>
2018-01-13 16:03:57 -0500

    Lower profitability threshold

--- scripts/trader_02_single.js
+++ scripts/trader_02_single.js
@@ -245,7 +245,7 @@ const checkProfitability = (soldCoin, boughtCoin, valueCoin, frozenStatus) => {
         return false;
     }
 
-    return percentChangeSum > 0.25;
+    return percentChangeSum > 0.01;
 };
 
 const makeTrade = async(soldCoin, boughtCoin, frozenStatus) => {
