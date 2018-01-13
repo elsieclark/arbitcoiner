5d07900f91e77b7cfcd8fa1aa3a9864cf77d6113
Will Clark <willdavidc@gmail.com>
2018-01-12 20:33:47 -0500

    Add error logging

--- scripts/recon_02.js
+++ scripts/recon_02.js
@@ -67,11 +67,11 @@ poloniex.on('open', () => {
 });
 
 poloniex.on('close', (reason, details) => {
-    console.log(`Poloniex WebSocket connection disconnected`);
+    console.log(`Poloniex WebSocket connection disconnected`, reason, details);
 });
 
 poloniex.on('error', (error) => {
-    console.log(`An error has occured`);
+    console.log(`An error has occured`, error);
 });
 
 poloniex.openWebSocket({ version: 2 });
